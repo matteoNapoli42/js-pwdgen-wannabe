3c7a26fec71e22daf25246551010bf578bf0a72d
matteoNapoli42 <matteon4242@gmail.com>
2023-08-22 16:03:11 +0200

    finished the writing on file

---
+++ assets/script/main.js
@@ -0,0 +1,11 @@
+let nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+let cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome);
+let colore = prompt("Inserisci il tuo colore preferito");
+console.log(colore);
+
+let password = nome+cognome+colore+23;
+
+document.getElementById("display").innerHTML = nome+cognome+colore+23;
+
